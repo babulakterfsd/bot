32f9bb0dd98f2e4d28f3ddbc4eda1229b092c9a3
Babul Akter <babulakterfsd@gmail.com>
2022-04-10 22:58:40 +0600

    bot status part done with responsiveness

--- src/components/dashboard/BotStatus.jsx
+++ src/components/dashboard/BotStatus.jsx
@@ -3,9 +3,34 @@ import styles from '../../styles/BotStatus.module.css';
 
 function BotStatus() {
     return (
-        <div className={`${styles.BotStatus} bg-reddishWhite px-4 py-8 -mt-16`}>
-            <div>
-                <p>this is bot status</p>
+        <div>
+            <div className={`${styles.BotStatus} bg-[#1d1726] px-4 py-8 -mt-16 mb-6`}>
+                <h1 className="font-montserrat font-bold text-2xl mb-8">Bot status</h1>
+                <div className="statusboxes grid grid-cols-12 space-y-1">
+                    <div className="runeprice col-span-12 py-2 pl-4 bg-black flex items-center rounded-md">
+                        <span className="mr-4">RUNE price : </span>
+                        <span className="text-offReddish font-semibold text-lg">${14.2}</span>
+                    </div>
+                    <div className="runeprice col-span-12 py-2 pl-4 bg-black flex items-center rounded-md">
+                        <span className="mr-4"># of RUNE : </span>
+                        <span className="text-offReddish font-semibold text-lg">{1247}</span>
+                    </div>
+                    <div className="runeprice col-span-12 py-2 pl-4 bg-black flex items-center rounded-md">
+                        <span className="mr-4">Profit : </span>
+                        <span className="text-offReddish font-semibold text-lg">${25983.0}</span>
+                    </div>
+                </div>
+            </div>
+            <div className="quickaction bg-[#2d2336] shadow-lg rounded-lg w-full -mt-8 p-4">
+                <h2 className="text-center font-montserrat font-bold text-2xl">Quick Actions</h2>
+                <h6 className="text-center font-montserrat text-sm mt-4 mb-8">
+                    Sell 25% of position
+                </h6>
+                <div className="text-center">
+                    <button type="button" className="dashboard-primary-btn ">
+                        More
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -83,10 +83,10 @@ function Dashboard() {
                 </div>
                 {/* dashboard */}
                 <div className="grid grid-cols-12 gap-x-4 lg:gap-y-12 lg:px-32 min-h-screen mt-12 lg:mt-0">
-                    <div className="col-span-12 lg:col-span-3 px-2 lg:px-0 mb-28 lg:mb-0">
+                    <div className="col-span-12 lg:col-span-4 px-2 lg:px-0 mb-28 lg:mb-0">
                         <BotStatus />
                     </div>
-                    <div className="col-span-12 lg:col-span-9 px-2 lg:px-0">
+                    <div className="col-span-12 lg:col-span-8 px-2 lg:px-0">
                         <BotSetting />
                     </div>
                     <div className="col-span-12 mt-32 px-2 lg:px-0 mb-8 lg:mb-0">
