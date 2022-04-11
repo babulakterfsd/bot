6aa266e751510d19d0259ed3e3eddac3246831a4
Babul Akter <babulakterfsd@gmail.com>
2022-04-12 01:15:31 +0600

    botsetting design completed

--- src/components/dashboard/BotSetting.jsx
+++ src/components/dashboard/BotSetting.jsx
@@ -6,13 +6,16 @@ import styles from '../../styles/BotSetting.module.css';
 
 function BotSetting() {
     const [toggle, setToggle] = useState(true);
+    const [leverage, setLeverage] = useState(5);
     const toggleClass = 'transform translate-x-full';
 
     return (
         <div className={`${styles.BotSetting} bg-reddishDark py-9 px-2 lg:p-9 -mt-16 rounded-lg`}>
+            {/* heading and divider */}
             <h1 className="font-montserrat font-bold text-2xl mb-6">Settings</h1>
             <hr className="opacity-10" />
-            <div className="botswitcher my-9 flex">
+            {/* bot switcher */}
+            <div className="botswitcher my-12 flex items-center">
                 <div className="text-md font-semibold">Turn your bot on or off</div>
                 <div
                     className=" main-switcher md:w-14 md:h-7 w-12 h-6 flex items-center bg-[#53353c] rounded-full px-1 cursor-pointer ml-5 mr-9"
@@ -34,6 +37,7 @@ function BotSetting() {
                     )}
                 </div>
             </div>
+            {/* API Boxes */}
             <div className="apiboxes flex flex-col">
                 <div className="apikey flex flex-col lg:flex-row items-center">
                     <div className="apikeytext lg:w-[30%] mb-1 lg:mb-0">API Key : </div>
@@ -50,7 +54,37 @@ function BotSetting() {
                 <div className="subaccount-dropdown flex flex-col lg:flex-row  items-center">
                     <div className="apikeytext lg:w-[30%] lg:mb-0" />
                     <div className="bg-[#1b1423] w-full py-3 rounded-md text-sm col-span-9 lg:pl-10 text-center lg:text-left">
-                        <Dropdown />
+                        <Dropdown name="Subaccount name" />
+                    </div>
+                </div>
+            </div>
+            {/* leaverage */}
+            <div className="leverage my-12 flex items-center  flex-col lg:flex-row shadow-lg bg-[#1b1423] py-4 rounded-lg">
+                <div className="text-md font-semibold mx-auto ">Choose Your leverage</div>
+                <div className="range mx-4 my-5 lg:my-0 flex items-center">
+                    <input
+                        type="range"
+                        value={leverage}
+                        step={1}
+                        min={1}
+                        max={10}
+                        onChange={(e) => setLeverage(e.target.value)}
+                        className="rounded-lg accent-[#ff5754] cursor-pointer"
+                    />
+                    <span className="mx-3 px-2 py-0.5 bg-[#53353c] opacity-80">{leverage}</span>
+                </div>
+                <div className="mx-auto lg:ml-auto">
+                    <button type="button" className="dashboard-primary-btn-sm ml-2">
+                        Confirm
+                    </button>
+                </div>
+            </div>
+            {/* Choose coin */}
+            <div className="coin my-12 ">
+                <div className="apikey flex flex-col lg:flex-row items-center">
+                    <div className="apikeytext lg:w-[30%] mb-1 lg:mb-0">Choose coin : </div>
+                    <div className="bg-[#1b1423] w-full lg:w-64 py-3 rounded-md text-sm col-span-9 lg:pl-10 text-center lg:text-left">
+                        <Dropdown name="Coin" />
                     </div>
                 </div>
             </div>
