410badc154aa2edd908a3ed5828ea6f0d30ab5c7
netstermonir <monirhossain6163@gmail.com>
2022-04-17 22:54:38 +0600

    api call

--- src/components/dashboard/MainTab/MainTab.jsx
+++ src/components/dashboard/MainTab/MainTab.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, TabList, Tabs } from 'react-tabs';
-import TabPanel from 'react-tabs/lib/components/TabPanel';
 import 'react-tabs/style/react-tabs.css';
 import styles from '../../../styles/MainTab.module.css';
-import TabDash from '../Tab/TabDash';
 
 function MainTab() {
+    const [Allbot, setAllbot] = useState([]);
+    console.log(Allbot);
+    useEffect(() => {
+        fetch('https://pipesai.mpact-labs.com/get_all_bots')
+            .then((res) => res.json())
+            .then((data) => {
+                setAllbot(data);
+            });
+    }, []);
+
     return (
         <div className="main-container">
             <Tabs className=" px-2 lg:px-0">
                 <TabList className="flex flex-col lg:flex-row">
-                    <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Bot 1</Tab>
-                    <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Bot 2</Tab>
+                    {Allbot?.map((bot, index) => (
+                        <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>
+                            Bot{index + 1}
+                        </Tab>
+                    ))}
+                    {/* <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Bot 2</Tab>
                     <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Bot 3</Tab>
-                    <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Add New Bot</Tab>
+                    <Tab className={`${styles.tabbtn} col-span-12 lg:col-span-4`}>Add New Bot</Tab> */}
                 </TabList>
-                <TabPanel>
+
+                {/* <TabPanel>
                     <TabDash />
                 </TabPanel>
                 <TabPanel>
                     <TabDash />
-                    <h2>Bot 2 tab</h2>
-                </TabPanel>
-                <TabPanel>
-                    <TabDash />
-                    <h2>Bot 3 tab</h2>
-                </TabPanel>
+                </TabPanel> */}
             </Tabs>
         </div>
     );
